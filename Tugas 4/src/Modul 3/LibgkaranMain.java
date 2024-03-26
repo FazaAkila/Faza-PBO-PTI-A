2a407a26812013292cb61866161c49c1faa6151c
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2024-03-26 23:25:38 +0700

    Delete Tugas 4/src/Modul 3/LibgkaranMain.java

--- Tugas 4/src/Modul 3/LibgkaranMain.java
+++
@@ -1,9 +0,0 @@
-public class LingkaranMain{
-    public static void main(String[] args){
-        Lingkaran l = new Lingkaran(3);
-        l.setTinggi(10);
-        l.displayMessage();
-        Lingkaran l2 = new Lingkaran(4, 10);
-        l2.displayMessage();
-    }
-}

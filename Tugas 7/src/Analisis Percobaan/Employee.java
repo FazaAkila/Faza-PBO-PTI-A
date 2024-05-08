f5aa87576e580766406883ca7524b17bd06ff17e
FazaAkila <79125027+FazaAkila@users.noreply.github.com>
2024-05-08 22:47:32 +0700

    Create Employee.java

---
+++ Tugas 7/src/Analisis Percobaan/Employee.java
@@ -0,0 +1,35 @@
+import java.util.Date;
+
+public abstract class Employee {
+    private String name;
+    private String noKTP;
+    private Date birthDate; // Tanggal lahir karyawan
+
+    public Employee(String name, String noKTP, Date birthDate) {
+        this.name = name;
+        this.noKTP = noKTP;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNoKTP() {
+        return noKTP;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String toString() {
+        return String.format("Name: %s\nNo. KTP: %s\nBirth Date: %s", getName(), getNoKTP(), getBirthDate());
+    }
+
+    public abstract double earnings(); // Pendapatan
+}
